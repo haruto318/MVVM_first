b9b2be9c1fd7b8e4c9b5ba087d021f8595b8b030
Haruto Hamano <haruto.yhs318@gmail.com>
2024-05-26 19:14:52 +0900

    add Model

--- MVVM_first/Model/User.swift
+++ MVVM_first/Model/User.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+final class User{
+    let id: Int
+    let name: String
+    let iconUrl: String
+    let webURL: String
+    
+    init(attributes: [String: Any]){
+        id = attributes["id"] as! Int
+        name = attributes["login"] as! String
+        iconUrl = attributes["avatar_url"] as! String
+        webURL = attributes["html_url"] as! String
+    }
+}
