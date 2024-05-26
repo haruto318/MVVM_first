750b7d02985e130a710f24534c02190a78769a0a
Haruto Hamano <haruto.yhs318@gmail.com>
2024-05-26 18:13:44 +0900

    Initial Commit

---
+++ MVVM_first/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_first
+//
+//  Created by 濱野遥斗 on 2024/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
