853e3b7eb4d9ca96f12a9f310674b5ce72b23e6f
Haruto Hamano <haruto.yhs318@gmail.com>
2024-05-31 22:19:49 +0900

    applied MVVM

---
+++ MVVM_first/View/TimeLineCell.swift
@@ -0,0 +1,8 @@
+//
+//  TimeLineCell.swift
+//  MVVM_first
+//
+//  Created by 濱野遥斗 on 2024/05/31.
+//
+
+import Foundation
---
+++ MVVM_first/ViewModel/UserListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserListViewModel.swift
+//  MVVM_first
+//
+//  Created by 濱野遥斗 on 2024/05/31.
+//
+
+import Foundation
